3851e3799da2b8b69a0697e5f595df8b683d419b
Vinoth umasankar <vinoth.umasankar@ennvee.net>
2025-09-25 11:18:15 +0530

    ASN List bug fixed

--- src/screens/AsnReceivedScreen.js
+++ src/screens/AsnReceivedScreen.js
@@ -318,7 +318,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0,
   },
 
-  colItem: { flex: 2, paddingLeft: 8 },
+  colItem: { flex: 1 },
   colOrdered: { flex: 1 },
   colShipped: { flex: 1 },
   colReceiving: { flex: 1 },
