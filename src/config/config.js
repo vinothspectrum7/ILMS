a028150d37db3b1fbf734939a367bf2b333d99b6
Swekin_30 <swekin.johny@ennvee.net>
2026-01-12 22:08:31 +0530

    Ship_CSS updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 // Fusion API
 export const BASE_URL ='http://3.17.31.222:7002';
+// export const BASE_URL ='http://3.17.31.222:7003';
+
 // Local AWS
 // export const BASE_URL ='http://3.17.31.222:8000';
 // export const BASE_URL ='http://10.72.180.16:8000';
